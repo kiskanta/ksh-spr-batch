56416f84179f2a12f0fbf74d58d69c9d2e6a80ea
kiskanta <springkishore5@gmail.com>
2021-06-07 18:29:37 +0530

    Spring Batch Explore

---
+++ src/main/java/ksh/batch/kshsprbatch/KshSprBatchApplication.java
@@ -0,0 +1,13 @@
+package ksh.batch.kshsprbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KshSprBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KshSprBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ksh/batch/kshsprbatch/KshSprBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ksh.batch.kshsprbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KshSprBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
